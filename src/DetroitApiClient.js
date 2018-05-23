c72575fdbb9bf7a962a2c5208c08b2cbab9cbeee
Michael Evans <magic6435@gmail.com>
2018-05-23 11:22:13 -0400

    Add jest, remove fetch from main file, and setup babel

--- src/DetroitApiClient.js
+++ src/DetroitApiClient.js
@@ -1,25 +1,23 @@
-const axios = require('axios');
-const getDetailsFor = require('./utils/getDetailsFor')
-const getDemolitions = require('./utils/getDemolitions');
-const moment = require('moment');
+const getDetailsFor = require("./utils/getDetailsFor");
+const getDemolitions = require("./utils/getDemolitions");
+const moment = require("moment");
 
-const WASTE_API = "https://apis.detroitmi.gov/waste_notifier/address/"
-const WASTE_FORMAT = "/?format=json"
+require("es6-promise").polyfill();
+require("isomorphic-fetch");
 
+const WASTE_API = "https://apis.detroitmi.gov/waste_notifier/address/";
+const WASTE_FORMAT = "/?format=json";
 
-
-class DetroitApiClient{
-
+class DetroitApiClient {
   // Permit status
 
-
-  constructor(){
+  constructor() {
     this.status = {
       OPEN: "OPEN",
       CLOSED: "CLOSED",
       EXPIRED: "EXPIRED",
       ALL: "ALL"
-    }
+    };
 
     this.trashType = {
       BULK: "bulk",
@@ -27,73 +25,88 @@ class DetroitApiClient{
       TRASH: "trash",
       YARD: "yard",
       ALL: "all"
-    }
+    };
   }
 
   // Demolitons defaults to 200 meters (656feet).
   // The health dept standard is 400 feet.
-  demolitions(params, range = 200){
-    if(params.address){
+  demolitions(params, range = 200) {
+    if (params.address) {
       return getDetailsFor(params.address).then(details => {
         const lat = details.location.y;
         const long = details.location.x;
-        return getDemolitions({'lat': lat, 'long': long}, range);
-      })
-    }else{
+        return getDemolitions({ lat: lat, long: long }, range);
+      });
+    } else {
       const lat = params.location.lat;
       const long = params.location.long;
-      return getDemolitions({'lat': lat, 'long': long}, range);
+      return getDemolitions({ lat: lat, long: long }, range);
     }
   }
 
   // Waste defaults to trash.
-  waste(address, type = this.trashType.TRASH){
-    let url = WASTE_API + address + WASTE_FORMAT;
-    return axios.get(url)
-      .then(response => {return response.data})
-      .then(response => {
-        let pickups = response.next_pickups;
-        if(type != this.trashType.ALL){
-          return pickups[type]
-        }else{
+  waste(address, type = this.trashType.TRASH) {
+    const url = WASTE_API + address + WASTE_FORMAT;
+    return fetch(url)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        const pickups = data.next_pickups;
+        if (type != this.trashType.ALL) {
+          return pickups[type];
+        } else {
           return pickups;
         }
+      })
+      .catch(err => {
+        return "No address found";
       });
   }
 
-  blightTickets(address){
-    return getDetailsFor(address).then(details => {
-      const parcelId = details.attributes.User_fld
-      return `https://data.detroitmi.gov/resource/s7hj-n86v.json?parcelno=${parcelId}`
-    }).then(res => {
-      return axios.get(res).then(res => (res.data));
-    }).catch(err => ("Something went wrong."))
+  blightTickets(address) {
+    return getDetailsFor(address)
+      .then(details => {
+        const parcelId = details.attributes.User_fld;
+        return `https://data.detroitmi.gov/resource/s7hj-n86v.json?parcelno=${parcelId}`;
+      })
+      .then(res => {
+        return axios.get(res).then(res => res.data);
+      })
+      .catch(err => "Something went wrong.");
   }
 
-  permits(address, status = this.status.OPEN){
-    return getDetailsFor(address).then(details => {
-      const parcelId = details.attributes.User_fld
-      if(status == "ALL"){
-        return `https://data.detroitmi.gov/resource/but4-ky7y.json?parcel_no=${parcelId}`
-      }else{
-        console.log(`https://data.detroitmi.gov/resource/but4-ky7y.json?parcel_no=${parcelId}&permit_status=${status}`)
-        return `https://data.detroitmi.gov/resource/but4-ky7y.json?parcel_no=${parcelId}&permit_status=${status.toString()}`
-      }
-    }).then(res => {
-      return axios.get(res).then(response => {
-        let object = {"address": address, "permits": response.data}
-        return object
+  permits(address, status = this.status.OPEN) {
+    return getDetailsFor(address)
+      .then(details => {
+        const parcelId = details.attributes.User_fld;
+        if (status == "ALL") {
+          return `https://data.detroitmi.gov/resource/but4-ky7y.json?parcel_no=${parcelId}`;
+        } else {
+          console.log(
+            `https://data.detroitmi.gov/resource/but4-ky7y.json?parcel_no=${parcelId}&permit_status=${status}`
+          );
+          return `https://data.detroitmi.gov/resource/but4-ky7y.json?parcel_no=${parcelId}&permit_status=${status.toString()}`;
+        }
+      })
+      .then(res => {
+        return fetch(res)
+          .then(response => {
+            return response.json();
+          })
+          .then(data => {
+            return { address: address, permits: data };
+          });
       });
-    });
   }
 
   // This is just sugar on getParcelFor
-  parcelNumber(address){
-    const details = getDetailsFor(address)
-    details.attributes.User_fld
+  parcelNumber(address) {
+    const details = getDetailsFor(address);
+    details.attributes.User_fld;
   }
 }
 
-module.exports = function(){
+module.exports = function() {
   return new DetroitApiClient();
-}
+};
